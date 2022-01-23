776f58b35c81cccaa7d093e733a9684922c49ddb
denin <navdeep.beniwal1997@gmail.com>
2022-01-23 19:05:29 +0530

    feat : Add request logging & error handling on router and data-repository

--- recipe-repository.js
+++ recipe-repository.js
@@ -3,43 +3,127 @@ const fs = require('fs');
 const fileContent = fs.readFileSync('data.json', 'utf-8');
 const parsedJsonData = JSON.parse(fileContent);
 
-let recipes = parsedJsonData.recipes;
-let recipesSize = recipes.length;
+let recipes = parsedJsonData.recipes || [];
 
-function getRecipiesSize() {
+const getRecipiesSize = () => {
     return recipes.length;
 }
 
-const findAllRecipes = () => {
+const areRecipesEmpty = () => {
+    if (recipes === null || recipes === undefined || recipes.length <= 0) {
+        const error = new Error('Sorry! No Recipes are currently present');
+        error.status = 200; // since its due to lack of data
+        throw error;
+    }
+}
+
+function findAllRecipes() {
+    areRecipesEmpty();
     return recipes;
 }
 
-const findRecipeById = recipeId => {
-    for(let recipeIndex in recipes){
-        const recipe = recipes[recipeIndex];
-        if(recipe.recipeId === recipeId){
-            return recipe;
-        }
+const findLatestRecipes = size => {
+    areRecipesEmpty();
+
+    let locRecipes = recipes;
+    locRecipes.sort((a, b) => new Date(a.publishedDate) < new Date(b.publishedDate) ? 1 : -1);
+    
+    // returns a default array
+    if(size === null || size === undefined || size===-1) { 
+        if(locRecipes.length > 10) { return locRecipes.slice(0, 10); }
+        else { return locRecipes; }
     }
-    return null;
+
+    return locRecipes.slice(0, size);  
 }
 
-const saveRecipe = recipe => {
-    recipe['recipeId'] = ++recipesSize;
-    recipes.push(recipe);
+const findTrendingRecipes = size => {
+    areRecipesEmpty();
+
+    let locRecipes = recipes;
+    locRecipes.sort((a, b) => calculateListAvg(a.ratingsList) < calculateListAvg(b.ratingsList) ? 1 : -1);
+
+    if(size === null || size === undefined || size===-1) { 
+        if(locRecipes.length > 10) { return locRecipes.slice(0, 10); }
+        else { return locRecipes; }
+    }
+
+    return locRecipes.slice(0, size);
+}
+
+const findRecipeById = recipeId => {
+    areRecipesEmpty();
+        
+    const recipe = recipes.find(r => Number(r.recipeId) === Number(recipeId));
+    if (recipe === undefined || recipe === null) {
+        const error = new Error(`Recipe with the given id : ${recipeId} not found`);
+        error.status = 404; // since its due to lack of data
+        throw error;
+    }
+
     return recipe;
 }
 
+const saveRecipe = recipe => {
+    // throws error if validation fails
+    validateRecipeData(recipe);
+
+    const recipeToStore = prepareRecipeData(recipe);
+    recipes.push(recipeToStore);
+    return recipeToStore;
+}
+
+const validateRecipeData = data => {
+    if(!(data.hasOwnProperty("title"))){
+        const error = new Error('Title attribute is missing in the save request');
+        error.status = 400;
+        throw error;
+    }
+
+    if(!(data.hasOwnProperty("sourceBaristaId"))){
+        const error = new Error('SourceBaristaId is missing in the save request');
+        error.status = 400;
+        throw error;
+    }
+
+    if(!(data.hasOwnProperty("sourceBarista"))){
+        const error = new Error('SourceBarista is missing in the save request');
+        error.status = 400;
+        throw error;
+    }
+
+    // TODO: Need to check if any additional validation is needed here
+}
+
+// prepares Recipe data to be stored in array/database
+const prepareRecipeData = data => {
+    return {
+        recipeId : (data.recipeId && data.recipeId > getRecipiesSize()) ? data.recipeId : getRecipiesSize() + 1,
+        title : data.title,
+        sourceBaristaId : data.sourceBaristaId,
+        sourceBarista : data.sourceBarista,
+        description : data.description || null,
+        currentRating : data.currentRating || null,
+        imageUrl : data.imageUrl || null,
+        timeToMake : data.timeToMake || null,
+        brewType : data.brewType || null,
+        publishedDate : new Date().toISOString(),
+        calories : data.calories || null,
+        ingredients : data.ingredients || [],
+        labels : data.labels || [],
+        ratingsList : data.ratingsList || [],
+        intakeAmount : data.intakeAmount || null,
+        caffineAmountLabel : data.caffineAmountLabel || null,
+        instructions : data.instructions || []
+    }
+}
+
 const updateRecipe = (recipeId, recipeBody) => {
-    if(recipeId === undefined || recipeId === null)
-        return null;
-
-    if(recipeBody === undefined || recipeBody === null)
-        return null;
-
     const recipeIndex = recipes.findIndex(r => r.recipeId === recipeId);
     if(recipeIndex === -1){
-        return null;
+        const error = new Error(`Recipe with id : ${recipeId} not found.`);
+        error.status = 404;
+        throw error;
     }
 
     recipeBody.id = recipeId;
@@ -49,34 +133,13 @@ const updateRecipe = (recipeId, recipeBody) => {
 
 const deleteRecipeById = recipeId => {
     const recipeIndex = recipes.findIndex(r => r.recipeId === recipeId);
-    if(recipeIndex === -1)
-        return null;
+    if(recipeIndex === -1) {
+        const error = new Error(`Recipe with id : ${recipeId} not found.`);
+        error.status = 404;
+        throw error;
+    }
 
     return recipes.splice(recipeIndex, 1);
-}
-
-const findLatestRecipes = size => {
-    let locRecipes = recipes;
-    locRecipes.sort((a, b) => new Date(a.publishedDate) < new Date(b.publishedDate) ? 1 : -1);
-    
-    if(size === null || size === undefined) { return []; }
-
-    if(size !== -1) { return locRecipes.slice(0, size); }
-        
-    if(locRecipes.length > 10) { return locRecipes.slice(0, 10); }
-    else { return locRecipes; }
-}
-
-const findTrendingRecipes = size => {
-    let locRecipes = recipes;
-    locRecipes.sort((a, b) => calculateListAvg(a.ratingsList) < calculateListAvg(b.ratingsList) ? 1 : -1);
-
-    if(size === null || size === undefined) { return []; }
-
-    if(size !== -1) { return locRecipes.slice(0, size); }
-        
-    if(locRecipes.length > 10) { return locRecipes.slice(0, 10); }
-    else { return locRecipes; }
 }
 
 const calculateListAvg = list => list.reduce((a, b) => a+b) / list.length;
@@ -93,7 +156,3 @@ module.exports = {
         updateRecipe : updateRecipe
     }
 }
-
-
-/* =======================           TESTING ZONE               ============================== */
-
